8d91a82a58de77bcd9c1b10efa5d01f26ebd056d
SeaTrutle <facultya1479@gmail.com>
2022-06-18 15:00:20 +0900

    코드 추가

--- td.cpp
+++ td.cpp
@@ -2,67 +2,99 @@
 #include <iostream>
 #include <cstring>
 #include <cstdarg>
+#include <direct.h>
+#include <windows.h>
 
 #define SAVEDIR "./.dirList"
+#define dirwidth 80
+#define aliaswidth 20
+#define DIRSIZE MAX_PATH
 
 using namespace std;
 
-int writeDir();
-int readDir();
-int delDir();
+int write_dir();
+int read_dir();
+int delete_dir();
+int select_dir(int);
+int select_dir(string);
 
 int main(int argc, char *argv[], char *envp[])
 {
-
+        read_dir();
+        select_dir(3);
 
         return 0;
 }
 
-int writeDir()
+int write_dir()
 {
         int i;
-        string buff;
+        string dirbuff;
+        string aliasbuff;
 
         ofstream outFile;
         ifstream inFile;
 
-        outFile.open(SAVEDIR, ios::out | ios::app);
+        outFile.open(SAVEDIR, ios::app);
         if (outFile.is_open()==false) 
         {
-                cout << "Unable to open the file" << endl;
+                cout << "File open failed" << endl;
                 return -1;
         }
 
         cout << "File dir : ";
-        
-        cin >> buff;
-        outFile << buff << endl;
-
+        cin >> dirbuff;
         cout << "File alias : ";
 
-        cin >> buff;
-        outFile << buff << endl;
+        cin >> aliasbuff;
+        outFile << aliasbuff << " ";
+        outFile << dirbuff << endl;
 
         outFile.close();
 
         return 0;
 }
 
-int readDir()
+int read_dir()
 {
         ifstream inFile;
+        string s;
+        bool line = true;
 
         inFile.open(SAVEDIR);
         if(inFile.fail())
         {
-                cout << "Unable to open the file" << endl;
+                cout << "File open failed" << endl;
                 return -1;
         }
+        cout.width(aliaswidth/2 + 3);
+        cout << "ALIAS";
+        cout.width(8);
+        cout << "|";
+        cout.width(dirwidth/2 + 2);
+        cout << "DIR" << endl;
+        cout.width(dirwidth+aliaswidth+4);
+        cout.fill('-');
+        cout << "" << endl;
+        cout.fill(' ');
+
         while(!inFile.eof())
         {
-                string s;
                 inFile >> s;
-                cout << s << endl;
+                if(line)
+                {
+                        cout.width(aliaswidth);
+                        cout << left << s;
+                        line = false;
+                }
+                else
+                {
+                        cout.width(4);
+                        cout << "|";
+                        cout.width(dirwidth);
+                        cout << s << endl;
+                        line = true;
+                }
         }
 
         inFile.close();
@@ -70,25 +102,58 @@ int readDir()
         return 0;
 }
 
-int delDir()
+int delete_dir()
 {
         ifstream tempFile;
         ofstream outFile;
         tempFile.open(SAVEDIR);
         if(tempFile.fail())
         {
-                cout << "Unable to open file" << endl;
+                cout << "File open failed" << endl;
                 return -1;
         }
 
         if(outFile.fail())
         {
-                cout << "Unable to open file" << endl;
+                cout << "File open failed" << endl;
                 return -1;
         }
         
         tempFile.close();
         outFile.close();
+
+        return 0;
+}
+
+int select_dir(int select)
+{
+        ifstream inFile;
+        string dir;
+        char cp[DIRSIZE] = "cd ";
+        int i = 0;
+        inFile.open(SAVEDIR);
+        if(inFile.fail())
+        {
+                cout << "File open failed" << endl;
+                return -1;
+        }
+
+        while(i < select*2)
+        {
+                i++;
+                inFile >> dir;
+        }
+
+        cout << dir << endl;
+        system("dir");
+        chdir("c:\\");
+        system("dir");
+        if(GetCurrentDirectory(DIRSIZE, (LPSTR)(LPCWSTR)dir.c_str()) == -1)
+        {
+                cout << "Dir move failed" << endl;
+                return -1;
+        }
+        
 
         return 0;
 }
